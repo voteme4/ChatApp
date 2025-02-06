b8598fa0a03b41701ab353338d3c00ab257973bf
voteme4 <zachberg2004>
2025-02-06 18:26:50 -0500

    Auth controller finished

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+dotenv.config();
+
+/* app.get('/', (req, res) => {
     // root route http://localhost:${PORT}
     res.send('Sup bro!');
-});
+}); */
+
+app.use(express.json()); // middleware
 
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}!`);
+});
